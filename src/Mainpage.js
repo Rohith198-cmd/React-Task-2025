17dce52e26baec908758437d92999a19ece8918a
Rohith198-cmd <rohithpalani67@gmail.com>
2025-01-10 13:11:02 +0530

    removed comments

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -12,9 +12,6 @@ const Mainpage=()=>{
             <div className="Mainpage-content">
                 <img src={Profile} alt="Profile"  width={165}/>
             <p>My name is jhon</p>
-            {/* <p>{bio.city}</p>
-            <p>{bio.age}</p>
-            <p>{bio.gender}</p> */}
             <p>{`Age: ${bio.age}`}</p>
             <p>{`City: ${bio.city}`}</p>
             <p>{`Gender: ${bio.gender}`}</p>
